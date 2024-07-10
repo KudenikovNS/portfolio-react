bc5178dbd1ac6b1df787c163707c30a3a01539a6
Nikita Kudenikov <KudenikovNS@yandex.ru>
2024-07-10 14:18:40 +0200

    Fix: Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,23 +2,29 @@ import "./style.css";
 
 const Header = () => {
   return (
-    <header className='header'>
-      <div className='header__wrapper'>
-        <h1 className='header__title'>
+    <header className="header">
+      <div className="header__wrapper">
+        <h1 className="header__title">
           <strong>
             Hi, my name is <em>Nikita</em>
           </strong>
           <br />
-          This is my portfolio as a frontend developer
+          This is my portfolio as a <br/> frontend developer
         </h1>
-        <div className='header__text'>
-          <p>with passion for learning and creating.</p>
+        <div className="header__text">
+          <p>
+            * Due to the fact that the rights to the real commercial projects I
+            participated in belong to the companies I collaborated with, these
+            projects are not available for public demonstration. In my
+            portfolio, I showcase pet-projects that were created to demonstrate
+            my skills.
+          </p>
         </div>
         <a
-          href='https://drive.google.com/file/d/1M9XuyRGf9eyv9mSUkCbF8_urcXlZZFid/view?usp=sharing'
-          className='btn'
-          target='_blank'
-          rel='noreferrer'
+          href="https://drive.google.com/file/d/1M9XuyRGf9eyv9mSUkCbF8_urcXlZZFid/view?usp=sharing"
+          className="btn"
+          target="_blank"
+          rel="noreferrer"
         >
           Download CV
         </a>
