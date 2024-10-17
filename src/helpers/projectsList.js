daeccbe3363c05e8e5036458141f9f7da6d0d1e9
Nikita Kudenikov <KudenikovNS@yandex.ru>
2024-10-17 20:38:04 +0200

    Change: all projects

--- src/helpers/projectsList.js
+++ src/helpers/projectsList.js
@@ -7,180 +7,77 @@ import project06 from "./../images/projects/projects_06.jpg";
 import project07 from "./../images/projects/projects_07.jpg";
 import project08 from "./../images/projects/projects_08.jpg";
 import project09 from "./../images/projects/projects_09.jpg";
-import project10 from "./../images/projects/projects_10.jpg";
-import project11 from "./../images/projects/projects_11.jpg";
-import project12 from "./../images/projects/projects_12.jpg";
-import project13 from "./../images/projects/projects_13.jpg";
-import project14 from "./../images/projects/projects_14.jpg";
-import project15 from "./../images/projects/projects_15.jpg";
-import project16 from "./../images/projects/projects_16.jpg";
-import project17 from "./../images/projects/projects_17.jpg";
-import project18 from "./../images/projects/projects_18.jpg";
-import project19 from "./../images/projects/projects_19.jpg";
-import project20 from "./../images/projects/projects_20.jpg";
 
 const projects = [
   {
     id: 1,
-    title: "How to learn",
-    skills: "HTML, CSS",
-    img: project01,
-    gitHubLink: "https://github.com/KudenikovNS/How-to-learn",
-    URL: "https://kudenikovns.github.io/How-to-learn/",
-  },
-  {
-    id: 2,
-    title: "Russian travel",
-    skills: "HTML, CSS, BEM Nested, Flexbox, Adaptive",
-    img: project02,
-    gitHubLink:
-      "https://github.com/KudenikovNS/Russian-travel/tree/main/russian-travel",
-    URL: "https://kudenikovns.github.io/Russian-travel/russian-travel/index.html",
-  },
-  {
-    id: 3,
-    title: "Mesto (JavaScript)",
-    skills:
-      "JavaScript, OOP, HTML, CSS, BEM Nested, Flexbox, Grid Layout, Adaptive, Webpack",
-    img: project03,
-    gitHubLink: "https://github.com/KudenikovNS/Mesto-test-project",
-    URL: "https://kudenikovns.github.io/Mesto-test-project/",
-  },
-  {
-    id: 4,
-    title: "Mesto (React)",
-    skills:
-      "React, JavaScript, OOP, HTML, CSS, BEM Nested, Flexbox, Gridbox, Adaptive, Webpack",
-    img: project04,
-    gitHubLink: "https://github.com/KudenikovNS/mesto-react-auth",
-    URL: "https://kudenikovns.github.io/mesto-react-auth/",
-  },
-  {
-    id: 5,
-    title: "Blog (GatsbyJS)",
-    skills: "GarsbyJS, React, GraphQL",
-    img: project05,
-    gitHubLink: "https://github.com/KudenikovNS/gatsby-first-project",
-    URL: "https://clever-pavlova-892bdc.netlify.app/",
-  },
-  {
-    id: 6,
-    title: "Diplom (+Backend)",
-    skills: "HTML, CSS, JS, React, Git, Express.js, mongoDB",
-    img: project06,
-    gitHubLink:
-      "https://github.com/KudenikovNS/movies-explorer-frontend/tree/level-3",
-    gitHubBackend:
-      "https://github.com/KudenikovNS/movies-explorer-api/tree/level-1",
-  },
-  {
-    id: 7,
     title: "Lamborghini",
     skills: "HTML, CSS, JavaScript, BEM Nested, Flexbox, Adaptive",
-    img: project07,
+    img: project01,
     gitHubLink: "https://github.com/KudenikovNS/lamborghini-super-trofeo",
     URL: "https://kudenikovns.github.io/lamborghini-super-trofeo/",
   },
   {
-    id: 8,
+    id: 2,
     title: "Psychologist Irina Dan",
     skills: "HTML, CSS, JavaScript, BEM Nested, Flexbox, Adaptive",
-    img: project08,
+    img: project02,
     gitHubLink: "https://github.com/KudenikovNS/Ira-Dan",
     URL: "http://irinadan.com/",
   },
   {
-    id: 9,
-    title: "Zoo",
-    skills: "HTML, CSS, JavaScript, BEM Nested, Flexbox, Adaptive",
-    img: project09,
-    gitHubLink: "https://github.com/KudenikovNS/online-zoo",
-    URL: "https://kudenikovns.github.io/online-zoo/pages/main/index.html",
-  },
-  {
-    id: 10,
+    id: 3,
     title: "Gallery",
     skills: "HTML, CSS, JavaScript",
-    img: project10,
+    img: project03,
     gitHubLink: "https://github.com/KudenikovNS/gallery",
     URL: "https://kudenikovns.github.io/gallery/",
   },
   {
-    id: 11,
+    id: 4,
     title: "Slider",
     skills: "HTML, CSS, JavaScript",
-    img: project11,
+    img: project04,
     gitHubLink: "https://github.com/KudenikovNS/slider",
     URL: "https://kudenikovns.github.io/slider/",
   },
   {
-    id: 12,
+    id: 5,
     title: "Mini Game",
     skills: "HTML, CSS, JavaScript",
-    img: project12,
+    img: project05,
     gitHubLink: "https://github.com/KudenikovNS/mini-game",
     URL: "https://kudenikovns.github.io/mini-game/",
   },
   {
-    id: 13,
+    id: 6,
     title: "Game of circles",
     skills: "HTML, CSS, JavaScript",
-    img: project13,
+    img: project06,
     gitHubLink: "https://github.com/KudenikovNS/game-of-circles",
     URL: "https://kudenikovns.github.io/game-of-circles/",
   },
   {
-    id: 14,
-    title: "Colors",
-    skills: "HTML, CSS, JavaScript",
-    img: project14,
-    gitHubLink: "https://github.com/KudenikovNS/colors",
-    URL: "https://kudenikovns.github.io/colors/",
-  },
-  {
-    id: 15,
-    title: "Employees",
-    skills: "React, TypeScript, Redux Toolkit, Express, Antd",
-    img: project15,
-    gitHubLink: "https://github.com/KudenikovNS/pet_project_employees",
-  },
-  {
-    id: 16,
+    id: 7,
     title: "Weather",
     skills: "Vue 3",
-    img: project16,
+    img: project07,
     gitHubLink: "https://github.com/KudenikovNS/weather_vue3",
     URL: "https://kudenikovns.github.io/weather_vue3/",
   },
   {
-    id: 17,
-    title: "Checkbox",
-    skills: "JavaScript",
-    img: project17,
-    gitHubLink: "https://github.com/KudenikovNS/checkboxForCustomer",
-    URL: "https://kudenikovns.github.io/checkboxForCustomer/",
-  },
-  {
-    id: 18,
-    title: "Task Manager",
-    skills: "Vue 3, JavaScript, HTML, CSS",
-    img: project18,
-    gitHubLink: "https://github.com/KudenikovNS/todo-task-vue",
-    URL: "https://main--incredible-souffle-ceedc0.netlify.app/",
-  },
-  {
-    id: 19,
+    id: 8,
     title: "Fairy Forest Parallax",
     skills: "HTML, CSS, JavaScript, GSAP",
-    img: project19,
+    img: project08,
     gitHubLink: "https://github.com/KudenikovNS/fairy-forest-parallax",
     URL: "https://kudenikovns.github.io/fairy-forest-parallax/",
   },
   {
-    id: 20,
+    id: 9,
     title: "Fitness Classes",
     skills: "React, Tailwind, Vite",
-    img: project20,
+    img: project09,
     gitHubLink: "https://github.com/KudenikovNS/gym-react",
     URL: "https://kudenikovns.github.io/gym-react/",
   },
