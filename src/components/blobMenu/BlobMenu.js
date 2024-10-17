584b7027a3f0dc082efd273b11139488d1cf6b42
Nikita Kudenikov <KudenikovNS@yandex.ru>
2024-10-17 22:50:58 +0200

    Fix: blobMenu

--- src/components/blobMenu/BlobMenu.js
+++ src/components/blobMenu/BlobMenu.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./style.css";
 
@@ -6,14 +6,20 @@ const BlobMenu = () => {
   const activLink = "nav-list__link nav-list__link_activ";
   const normalLink = "nav-list__link";
 
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
-  const [curveX, setCurveX] = useState(10);
-  const [curveY, setCurveY] = useState(0);
-  const [targetX, setTargetX] = useState(0);
-  const [xitteration, setXitteration] = useState(0);
-  const [yitteration, setYitteration] = useState(0);
   const [menuExpanded, setMenuExpanded] = useState(false);
+
+  const xRef = useRef(0);
+  const yRef = useRef(0);
+  const curveXRef = useRef(10);
+  const curveYRef = useRef(0);
+  const targetXRef = useRef(0);
+  const xIterationRef = useRef(0);
+  const yIterationRef = useRef(0);
+
+  const blobPathRef = useRef(null);
+  const blobRef = useRef(null);
+  const hamburgerRef = useRef(null);
+  const menuRef = useRef(null);
 
   useEffect(() => {
     const easeOutExpo = (
@@ -29,164 +35,158 @@ const BlobMenu = () => {
     const hoverZone = 150;
     const expandAmount = 20;
 
+    let animationFrameId;
+
     const svgCurve = () => {
-      if (curveX > x - 1 && curveX < x + 1) {
-        setXitteration(0);
+      if (Math.abs(curveXRef.current - xRef.current) < 1) {
+        xIterationRef.current = 0;
       } else {
         if (menuExpanded) {
-          setTargetX(0);
+          targetXRef.current = 0;
         } else {
-          setXitteration(0);
-          if (x > hoverZone) {
-            setTargetX(0);
+          if (xRef.current > hoverZone) {
+            targetXRef.current = 0;
           } else {
-            setTargetX(-(((60 + expandAmount) / 100) * (x - hoverZone)));
+            targetXRef.current = -(
+              ((60 + expandAmount) / 100) *
+              (xRef.current - hoverZone)
+            );
           }
         }
-        setXitteration((prevXitteration) => prevXitteration + 1);
+        xIterationRef.current += 1;
+        curveXRef.current = easeOutExpo(
+          xIterationRef.current,
+          curveXRef.current,
+          targetXRef.current - curveXRef.current,
+          100
+        );
       }
 
-      if (curveY > y - 1 && curveY < y + 1) {
-        setYitteration(0);
+      if (Math.abs(curveYRef.current - yRef.current) < 1) {
+        yIterationRef.current = 0;
       } else {
-        setYitteration(0);
-        setYitteration((prevYitteration) => prevYitteration + 1);
+        yIterationRef.current += 1;
+        curveYRef.current = easeOutExpo(
+          yIterationRef.current,
+          curveYRef.current,
+          yRef.current - curveYRef.current,
+          100
+        );
       }
-
-      setCurveX((prevCurveX) =>
-        easeOutExpo(xitteration, prevCurveX, targetX - prevCurveX, 100)
-      );
-      setCurveY((prevCurveY) =>
-        easeOutExpo(yitteration, prevCurveY, y - prevCurveY, 100)
-      );
 
       const anchorDistance = 200;
       const curviness = anchorDistance - 40;
 
-      const newCurve =
-        "M60," +
-        window.innerHeight +
-        "H0V0h60v" +
-        (curveY - anchorDistance) +
-        "c0," +
-        curviness +
-        "," +
-        curveX +
-        "," +
-        curviness +
-        "," +
-        curveX +
-        "," +
-        anchorDistance +
-        "S60," +
-        curveY +
-        ",60," +
-        (curveY + anchorDistance * 2) +
-        "V" +
-        window.innerHeight +
-        "z";
+      const newCurve = `M60,${window.innerHeight}H0V0h60v${
+        curveYRef.current - anchorDistance
+      }c0,${curviness},${curveXRef.current},${curviness},${
+        curveXRef.current
+      },${anchorDistance}S60,${curveYRef.current},60,${
+        curveYRef.current + anchorDistance * 2
+      }V${window.innerHeight}z`;
 
-      const blobPath = document.getElementById("blob-path");
-      if (blobPath) {
-        blobPath.setAttribute("d", newCurve);
+      if (blobPathRef.current) {
+        blobPathRef.current.setAttribute("d", newCurve);
       }
-
-      const blob = document.getElementById("blob");
-      if (blob) {
-        blob.style.width = curveX + 60 + "px";
+      if (blobRef.current) {
+        blobRef.current.style.width = `${curveXRef.current + 60}px`;
       }
-
-      const hamburger = document.querySelector(".hamburger");
-      if (hamburger) {
-        hamburger.style.transform =
-          "translate(" + curveX + "px, " + curveY + "px)";
+      if (hamburgerRef.current) {
+        hamburgerRef.current.style.transform = `translate(${curveXRef.current}px, ${curveYRef.current}px)`;
       }
-
-      window.requestAnimationFrame(svgCurve);
+      animationFrameId = window.requestAnimationFrame(svgCurve);
     };
 
-    window.addEventListener("mousemove", (e) => {
-      setX(e.clientX);
-      setY(e.clientY);
-    });
+    const handleMouseMove = (e) => {
+      xRef.current = e.clientX;
+      yRef.current = e.clientY;
+    };
 
-    document
-      .querySelector(".hamburger, .menu-inner")
-      .addEventListener("click", function () {
-        this.parentNode.classList.add("expanded");
-        setMenuExpanded(true);
-      });
+    const handleHamburgerClick = () => {
+      menuRef.current.classList.add("expanded");
+      setMenuExpanded(true);
+    };
 
-    document
-      .querySelector(".menu-inner")
-      .addEventListener("click", function () {
-        setMenuExpanded(false);
-        this.parentNode.classList.remove("expanded");
-      });
+    const handleMenuInnerClick = () => {
+      menuRef.current.classList.remove("expanded");
+      setMenuExpanded(false);
+    };
 
-    window.requestAnimationFrame(svgCurve);
+    window.addEventListener("mousemove", handleMouseMove);
+    const hamburgerEl = hamburgerRef.current;
+    const menuInnerEl = menuRef.current?.querySelector(".menu-inner");
+
+    if (hamburgerEl) {
+      hamburgerEl.addEventListener("click", handleHamburgerClick);
+    }
+    if (menuInnerEl) {
+      menuInnerEl.addEventListener("click", handleMenuInnerClick);
+    }
+
+    svgCurve();
 
     return () => {
-      window.removeEventListener("mousemove", () => {});
+      window.removeEventListener("mousemove", handleMouseMove);
+      if (hamburgerEl) {
+        hamburgerEl.removeEventListener("click", handleHamburgerClick);
+      }
+      if (menuInnerEl) {
+        menuInnerEl.removeEventListener("click", handleMenuInnerClick);
+      }
+      window.cancelAnimationFrame(animationFrameId);
     };
-  }, [curveX, curveY, menuExpanded, targetX, x, xitteration, y, yitteration]);
+  }, [menuExpanded]);
 
   return (
-    <>
-      <div id='menu' className={menuExpanded ? "expanded" : ""}>
-        <div className='hamburger'>
-          <div className='line' />
-          <div className='line' />
-          <div className='line' />
-        </div>
-        <div className='menu-inner'>
-          <ul className='nav-list'>
-            <li className='nav-list__item'>
-              <NavLink
-                to='/portfolio-react'
-                className={({ isActive }) =>
-                  isActive ? activLink : normalLink
-                }
-              >
-                Home
-              </NavLink>
-            </li>
-            <li className='nav-list__item'>
-              <NavLink
-                to='/skills'
-                className={({ isActive }) =>
-                  isActive ? activLink : normalLink
-                }
-              >
-                Skills
-              </NavLink>
-            </li>
-            <li className='nav-list__item'>
-              <NavLink
-                to='/contacts'
-                className={({ isActive }) =>
-                  isActive ? activLink : normalLink
-                }
-              >
-                Contacts
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-
-        <svg
-          version='1.1'
-          id='blob'
-          xmlns='http://www.w3.org/2000/svg'
-          xmlnsXlink='http://www.w3.org/1999/xlink'
-        >
-          <path
-            id='blob-path'
-            d='M60,500H0V0h60c0,0,20,172,20,250S60,900,60,500z'
-          />
-        </svg>
+    <div id="menu" ref={menuRef} className={menuExpanded ? "expanded" : ""}>
+      <div className="hamburger" ref={hamburgerRef}>
+        <div className="line" />
+        <div className="line" />
+        <div className="line" />
       </div>
-    </>
+      <div className="menu-inner">
+        <ul className="nav-list">
+          <li className="nav-list__item">
+            <NavLink
+              to="/portfolio-react"
+              className={({ isActive }) => (isActive ? activLink : normalLink)}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="nav-list__item">
+            <NavLink
+              to="/skills"
+              className={({ isActive }) => (isActive ? activLink : normalLink)}
+            >
+              Skills
+            </NavLink>
+          </li>
+          <li className="nav-list__item">
+            <NavLink
+              to="/contacts"
+              className={({ isActive }) => (isActive ? activLink : normalLink)}
+            >
+              Contacts
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <svg
+        version="1.1"
+        id="blob"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
+        ref={blobRef}
+      >
+        <path
+          id="blob-path"
+          ref={blobPathRef}
+          d="M60,500H0V0h60c0,0,20,172,20,250S60,900,60,500z"
+        />
+      </svg>
+    </div>
   );
 };
 
