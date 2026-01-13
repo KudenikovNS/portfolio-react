d11c1e25a1af26299fd7927f19b9b955d437af0a
KudenikovNS <kudenikov.92@gmail.com>
2026-01-13 16:58:34 +0100

    fix router

--- src/App.js
+++ src/App.js
@@ -27,12 +27,12 @@ function App() {
 
   return (
     <div className='App'>
-      <Router>
+      <Router basename="/portfolio-react">
         <ScrollToTop />
         <Logo />
         {windowWidth > 600 && <BlobMenu />}
         <Routes>
-          <Route path='/portfolio-react' element={<Projects />} />
+          <Route path='/' element={<Projects />} />
           <Route path='/skills' element={<Skills />} />
           <Route path='/project/:id' element={<Project />} />
           <Route path='/contacts' element={<Contacts />} />
